96cbcb2d699b878e34dc64d95d63f1d0c0efd339
j1997sh <jamesalhiggs@icloud.com>
2025-12-04 15:27:45 +0000

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,15 +1,18 @@
+// Initialize Firebase (Modular v10+)
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
+// Your Firebase configuration (westminsterbeer project)
 const firebaseConfig = {
-  apiKey: "AIzaSyBa9qyjAIlFil46dtKHNq99w6_gWDuOBLI",
+  apiKey: "AIzaSyCpa0ArQCMDMyzv8tSgYHNaDhqm22qzWf8",
   authDomain: "westminsterbeer.firebaseapp.com",
   projectId: "westminsterbeer",
-  storageBucket: "westminsterbeer.appspot.com",
-  messagingSenderId: "221558546438",
-  appId: "1:221558546438:web:3a4ab8d6ecf73d57fba387",
-  measurementId: "G-N8EJ2S5GKT"
+  storageBucket: "westminsterbeer.firebasestorage.app",
+  messagingSenderId: "961171696122",
+  appId: "1:961171696122:web:b43b517d17dcf778e5dc43",
+  measurementId: "G-KBTEYF6YRY"
 };
 
+// Init Firebase
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
